c1d673abf1e4ec58804befd14d39b210d61f8cc7
Dreican <dreican@users.noreply.github.com>
2026-09-13 14:08:05 +0200

    Add detailed event, event status, and user type definitions to events module

--- src/features/events/type/event_statues.ts
+++ src/features/events/type/event_statues.ts
@@ -11,3 +11,11 @@ export interface EventStatusSummaryResponse {
     code: EventStatusCode
     name: string
 }
+
+export interface EventStatusResponse extends EventStatusSummaryResponse {
+    description: string | null
+    is_default: boolean
+    sort_order: number
+    is_bookable: boolean
+    allow_edit: boolean
+}
--- src/features/events/type/events.ts
+++ src/features/events/type/events.ts
@@ -1,5 +1,12 @@
-import type {EventStatusSummaryResponse} from "@/features/events/type/event_statues.ts";
+import type {EventStatusResponse, EventStatusSummaryResponse} from "@/features/events/type/event_statues.ts";
 import type {ActivityTypeSummaryResponse} from "@/features/events/type/activity_types.ts";
+import type {UserSummaryResponse} from "@/features/users/types/users.ts";
+
+export enum EventAudience {
+    PUBLIC = "PUBLIC",
+    MEMBERS = "MEMBERS",
+    VIP = "VIP"
+}
 
 export interface EventSummaryResponse {
     id: string
@@ -16,3 +23,31 @@ export interface EventSummaryResponse {
     activity_type: ActivityTypeSummaryResponse
     status: EventStatusSummaryResponse
 }
+
+
+export interface EventResponse extends EventSummaryResponse {
+    description: string | null
+
+    capacity: number | null
+    banner_url: string | null
+    activity_type: ActivityTypeSummaryResponse
+    creator: UserSummaryResponse
+    status: EventStatusSummaryResponse
+
+    is_full: boolean
+}
+
+export interface EventAdminResponse extends EventResponse {
+    status: EventStatusResponse
+
+    audience: EventAudience
+
+    published_at: Date | null
+    cancelled_at: Date | null
+    completed_at: Date | null
+    created_at: Date
+    updated_at: Date
+    deleted_at: Date | null
+    deleted_by: UserSummaryResponse | null
+}
+
